2c484bc18315d846973383bef9cb8f6e71a7f68f
Rob Beffers <rob.beffers@rivm.nl>
2019-10-25 10:43:54 +0200

    href

--- gisportal/beheer/geo-package.php
+++ gisportal/beheer/geo-package.php
@@ -141,8 +141,10 @@ if ($loggedIn){
 				}
 				$tab1.='<tr><td>Soort:</td><td>'.$basicPage->getSelect('soort',$g['soort'],array('Raster','Vector')).'</td></tr>';
 				$tab1.='<tr><td colspan="2">&nbsp;</a></td></tr>';
-				$tab1.='<tr><td>Sqlite file (TIJDELIJK MOET JE -1 ACHTER DE EXTENSIE ZETTEN OMDAT PHP DE FILE ANDERS NIET UPLOAD!!:</td><td><span id="brongeopackage1" style="margin-right: 20px;">'.htmlspecialchars($g['brongeopackage']).'</span><input type="hidden" id="brongeopackage" name="brongeopackage" value="'.$g['brongeopackage'].'"><a id="geo-package-file-button" class="small-button" style="float: right;" uploadFile="geo-package,'.$g['id'].'">Upload geo-package</a></td></tr>';
-				$tab1.='<tr><td>qgs file:</td><td><span id="opmaak1" style="margin-right: 20px;">'.htmlspecialchars($g['opmaak']).'</span><input type="hidden" id="opmaak-file" name="opmaak" value="'.$g['opmaak'].'"><a id="opmaak-file-button" class="small-button" style="float: right;" uploadFile="sld,'.$g['id'].'">Upload file</a></td></tr>';
+				$tab1.='<tr><td>Upload een file:</td><td><span id="brongeopackage1" style="margin-right: 20px;">'.htmlspecialchars($g['brongeopackage']).'</span><input type="hidden" id="brongeopackage" name="brongeopackage" value="'.$g['brongeopackage'].'"><a id="geo-package-file-button" class="small-button" style="float: right;" uploadFile="geo-package,'.$g['id'].'">Upload geo-package</a></td></tr>';
+//				$tab1.='<tr><td>qgs file:</td><td><span id="opmaak1" style="margin-right: 20px;">'.htmlspecialchars($g['opmaak']).'</span><input type="hidden" id="opmaak-file" name="opmaak" value="'.$g['opmaak'].'"><a id="opmaak-file-button" class="small-button" style="float: right;" uploadFile="sld,'.$g['id'].'">Upload file</a></td></tr>';
+				$ext=$db->selectOne('versions','extensions','id='.$g['version']);
+				$tab1.='<tr><td>Files:</td><td>'.$ext['extentions'].'</td></tr>';
 				$tab1.='<tr><td colspan="2">&nbsp;</a></td></tr>';
 				if (file_exists('indata.data')) {
 					if (time()-filemtime('indata.data')>60*60) {unlink('indata.data');} // gooi weg als ouder dan een uur

3f81256cbd8b2e3e15c5a45a906da55ee628051a
Rob Beffers <rob.beffers@rivm.nl>
2019-10-28 10:03:26 +0100

    href

--- gisportal/beheer/geo-package.php
+++ gisportal/beheer/geo-package.php
@@ -68,9 +68,9 @@ if ($loggedIn){
 					if (!$db->foutMeldingen) {
 						$version=$db->selectOne('versions AS a LEFT JOIN images AS b ON b.id=a.image','b.image,a.version','a.id='.$a['version']);
 						$theme=$db->selectOne('onderwerpen','afkorting','id='.$a['onderwerp']);
+$basicPage->writeLog('$a='.var_export($a,true).'<br>$g='.var_export($g,true));
 						if ($g['id']==0) {
 							$g['id']=$db->insert('geopackages',$a);
-$basicPage->writeLog(var_export($a,true).' => '.$g['id']);
 							$openshift_api->createDeploymentConfig('../',$g['id'],$theme['afkorting'],$a['Qkaartnaam'],$version['image'],$version['version']);
 						} else {
 							$db->update('geopackages',$a,'id='.$g['id']);

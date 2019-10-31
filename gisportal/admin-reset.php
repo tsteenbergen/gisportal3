d9b3967489e5e77dc9864e32f3e9ef64ef8e254c
Rob Beffers <rob.beffers@rivm.nl>
2019-10-31 11:07:29 +0100

    commit

--- gisportal/admin-reset.php
+++ gisportal/admin-reset.php
@@ -6,8 +6,7 @@ $r='';
 if ($loggedIn && $is_admin) {
 	$r.='Bij een reset wordt voor elk van geo-packages die voldoet aan het filter, het volgende gedaan:<ol>';
 	$r.='<li>Als \'Verwijder uploads\' is aangevinkt, dan worden alle bestanden van de betreffende geo-package verwijderd. Deze zullen dus opnieuw moeten worden geupload.</li>';
-	$r.='<li>Op het containerplatform worden de volgende zaken verwijderd:</li>';
-	$r.='<li><ul>';
+	$r.='<li>Op het containerplatform worden de volgende zaken verwijderd:<ul>';
 	$r.='<li>replicationcontroller</li>';
 	$r.='<li>autoscaler</li>';
 	$r.='<li>deploymentconfig</li>';
@@ -17,31 +16,27 @@ if ($loggedIn && $is_admin) {
 	$r.='</ul></li>';
 	$r.='<li>Bovenstaande zaken worden vervolgens opnieuw aangemaakt (behalve de pods, die \'ontstaan vanzelf\' door de nieuwe deploymentconfig).</li>';
 	$r.='</ol>';
+	$r.='<br>Om de reset uit te voeren worden 3 stappen doorlopen:<ol><li>Filter instellen</li><li>Controle gevolgen</li><li>Uitvoering</li></ol>';
 	
 	$func=$_POST['func'];
-	$id=isset($_POST['id'])?(int)$_POST['id']:1;
-	// Betekenis van $id:
-	// 1 is toon de pagina waarop met het filter kan samenstellen
-	// 2 is toon de pagina opnieuw, maar geef nu aan wat de consequenties zijn
-	// 3 is: Voer e.e.a. uit en laat de voortgang zien. Als het gedaan is; Ga naar $id=1
-	if ($func=='opslaan') {
-		$id++;
-	}
-	$r.='<div style="display: inline-block;"><form id="form" method="POST"><input type="hidden" name="id" value="'.$id.'"><input type="hidden" name="func" id="func">';
-	switch ($id) {
-		case 1:
-			$r.='Stap 1: Samenstellen filter<table>';
+	$r.='<div style="display: inline-block;"><form id="form" method="POST">';
+	switch ($func) {
+		case 'controle':
+			$r.='<input type="hidden" name="func" value="uitvoeren">';
+			$r.='<h2>Stap 2: Controle gevolgen</h2><table>';
+			break;
+		case 'uitvoeren':
+			$r.='<input type="hidden" name="func" value="">';
+			$r.='<h2>Stap 3: Uitvoering</h2><table>';
+			break;
+		default:
+			$r.='<input type="hidden" name="func" value="controle">';
+			$r.='<h2>Stap 1: Filter instellen</h2><table>';
 			$r.='<tr><td colspan="2"><input type="checkbox" name="delete_files" id="delete_files"><label for="delete_files"> Verwijder uploads.</label></td></tr>';
-			break;
-		case 2:
-			$r.='Stap 2: Controle gevolgen<table>';
-			break;
-		case 3:
-			$r.='Stap 3: Uitvoeren<table>';
 			break;
 	}
 	$r.='</table></form>';
-	$r.='<div class="button-below"><button onclick="formOpslaan();">Opslaan</button></div></div>';
+	$r.='<div class="button-below"><button onclick="$(\'#form\').submit();">Opslaan</button></div></div>';
 	
 } else {
 	$basicPage->fout('Autorisatie','Je hebt niet de juiste autorisatie.');

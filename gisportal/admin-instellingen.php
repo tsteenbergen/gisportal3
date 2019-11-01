f81de6f48a96a137dbd33b3776b4eda0ac950dea
Rob Beffers <rob.beffers@rivm.nl>
2019-11-01 10:48:54 +0100

    commit

--- gisportal/admin-instellingen.php
+++ gisportal/admin-instellingen.php
@@ -10,12 +10,12 @@ if ($loggedIn && $is_admin) {
 
 
 	$func=$_POST['func'];
-	$instellingen=$db->select('instellingen','id,label,variable,instelling','id>=1');
+	$instellingen=$db->select('instellingen','id,label,var,instelling','id>=1');
 	if ($func=='opslaan') {
 		foreach ($_POST as $key=>$post) {
 			for ($t=0;$t<count($instellingen);$t++) {
 				$instelling=$instellingen[$t];
-				if ($instelling['variable']==$key) {
+				if ($instelling['var']==$key) {
 					$db->update('instellingen',array('Qinstelling'=>$post),'id='.$instelling['id']);
 				}
 			}
@@ -23,7 +23,7 @@ if ($loggedIn && $is_admin) {
 	}
 	$r.='<div style="display: inline-block;"><form id="form" method="POST"><input type="hidden" name="func" id="func"><table>';
 	foreach ($instellingen as $instelling) {
-		$r.='<tr><td>'.$instelling['label'].':</td><td><input name="'.$instelling['variable'].'" value="'.$instelling['instelling'].'" size="32"></td></tr>';
+		$r.='<tr><td>'.$instelling['label'].':</td><td><input name="'.$instelling['var'].'" value="'.$instelling['instelling'].'" size="32"></td></tr>';
 	}
 	$r.='</table></form>';
 	$r.='<div class="button-below"><button onclick="formOpslaan();">Opslaan</button></div>';

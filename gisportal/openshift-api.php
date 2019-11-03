22c42d70c5b648c2d0eee2f6b119e81ae5e81561
Rob Beffers <rob.beffers@rivm.nl>
2019-11-03 09:24:35 +0100

    commit

--- gisportal/openshift-api.php
+++ gisportal/openshift-api.php
@@ -178,7 +178,7 @@ class openshift_api_ {
 		$checkItems=[];
 		foreach ($todo_types as $todo_type) {
 			$todo=$this->def[$todo_type];
-			$this->command($todo['api'],$todo['type'].'?labelSelector=name=gpid-'.$id);
+			$this->command($todo['api'],$todo['type'].'?labelSelector=name=gpid-'.$id,'GET','{"includeUninitialized":true}');
 			if ($this->response->kind==$todo['array']) {
 				$items=[];
 				for ($t=0;$t<count($this->response->items);$t++) {
@@ -191,7 +191,7 @@ class openshift_api_ {
 			}
 		}
 		// Wacht tot alles echt verwijderd is
-/*		$maxAant=20; // wacht maximaal 20 seconden
+		$maxAant=20; // wacht maximaal 20 seconden
 		while (count($checkItems)>0 && $maxAant>0) {
 			for ($t=count($checkItems)-1;$t>=0;$t--) {
 				$item=$checkItems[$t];
@@ -204,7 +204,7 @@ class openshift_api_ {
 			$maxAant--;
 			// usleep(100000); // 100.000 microseconden is 0.1 seconde
 			sleep(1); // 1 seconde
-		}*/
+		}
 	}
 }
 

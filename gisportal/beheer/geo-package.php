d077de7582993441ea913fdb12cdc4376a4c28e4
Rob Beffers <rob.beffers@rivm.nl>
2019-11-07 19:27:11 +0100

    commit

--- gisportal/beheer/geo-package.php
+++ gisportal/beheer/geo-package.php
@@ -161,7 +161,7 @@ if ($loggedIn){
 				$tab1.='<tr><td>Naam:</td><td><input name="naam" value="'.htmlspecialchars($g['naam']).'" size="32"></td></tr>';
 				$tab1.='<tr><td>Kaartaam (in URL):</td><td><input name="kaartnaam" value="'.htmlspecialchars($g['kaartnaam']).'" size="32" onchange="regel_kaart_url();"></td></tr>';
 				$tab1.='<tr><td colspan="2">&nbsp;</td></tr>';
-				$tab1.='<tr><td>URL:</td><td><input style="width: 100%;" id="kaart-url" onchange="regel_kaart_url();"></td></tr>';
+				$tab1.='<tr><td>URL:</td><td><input size="60" id="kaart-url" onchange="regel_kaart_url();"></td></tr>';
 				$basicPage->add_js_ready('regel_kaart_url();');
 				if ($is_admin) {
 					$tab1.='<tr><td colspan="2">&nbsp;</a></td></tr>';

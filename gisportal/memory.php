a498d6833018329608d26f65ac354e85446f1d80
Rob Beffers <rob.beffers@rivm.nl>
2019-11-26 10:41:38 +0100

    commit

--- gisportal/memory.php
+++ gisportal/memory.php
@@ -21,7 +21,7 @@ class memory  {
 		
 		$openshift_api->command('GET','persistentvolumeclaim',['name'=>$basicPage->getConfig('persistent_storage')]);
 		//$this->persistent_afk=$openshift_api->response->spec->resources->requests->storage;
-		$this->persistent_afk=$openshift_api->response->phase->storage;
+		$this->persistent_afk=var_export($openshift_api->response,true);//->phase->storage;
 		foreach (['T','Ti','G','Gi','M','Mi','K','Ki'] as $b) if (substr($this->persistent_afk,-strlen($b))==$b) {
 			$this->error=false;
 			$this->persistent=intval(substr($this->persistent_afk,0,strlen($this->persistent_afk)-strlen($b)));
